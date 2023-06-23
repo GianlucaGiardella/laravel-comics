a19d2603de7573e87217ed085aca596010b81588
GianluGiarde <gianlugiarde@gmail.com>
2023-06-23 20:16:44 +0200

    Add: main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="{{ Vite::asset('resources/img/favicon.ico') }}" type="image/x-icon">
     @vite('resources/js/app.js')
-    <title>Template</title>
+    <title>Laravel DC Comics</title>
 </head>
 <body>
-    <h1>Template di base</h1>
+    @include("partials.header")
+    <main>
+        <div class="jumbotron"></div>
+          <div class="main-container">
+            <div class="current-series">
+              <h2>current series</h2>
+              <ul class="series-list">
+              </ul>
+              <button class="more">load more</button>
+            </div>
+        </div>
+      </main>
+    @include("partials.footer")
 </body>
 </html>
