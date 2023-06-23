9d55abc19548910ba60496ca00e8e50a1b7dd2e2
GianluGiarde <gianlugiarde@gmail.com>
2023-06-23 20:15:12 +0200

    Add: footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,107 @@
+<footer>
+    <div class="top-footer-bg">
+      <div class="footer-container">
+        <div class="top-footer">
+
+          <section>
+            <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="/buy comics digital img" />
+            <h2>digital comics</h2>
+          </section>
+
+          <section>
+            <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="buy comics merchandise img" />
+            <h2>dc merchandise</h2>
+          </section>
+
+          <section>
+            <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="buy comics subscriptions img " />
+            <h2>subscription</h2>
+          </section>
+
+          <section>
+            <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="buy comics shop locator img" />
+            <h2>comic shop locator</h2>
+          </section>
+
+          <section>
+            <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="buy dc power visa img " />
+            <h2>dc power visa</h2>
+          </section>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="middle-footer-bg">
+      <div class="footer-container">
+        <div class="middle-footer">
+          <div class="lists">
+
+            <ul class="comics">
+              <li><h2>dc comics</h2></li>
+              <li>Characters</li>
+              <li>Comics</li>
+              <li>Movies</li>
+              <li>TV</li>
+              <li>Games</li>
+              <li>Videos</li>
+              <li>News</li>
+            </ul>
+
+            <ul class="shop">
+              <li><h2>shop</h2></li>
+              <li>Shop DC</li>
+              <li>Shop DC Collectibles</li>
+            </ul>
+
+            <ul class="dc">
+              <li><h2>dc</h2></li>
+              <li>Terms Of Use</li>
+              <li>Privacy policy (New)</li>
+              <li>Ad Choices</li>
+              <li>Advertising</li>
+              <li>Jobs</li>
+              <li>Subscriptions</li>
+              <li>Talent Workshops</li>
+              <li>CPSC Certificates</li>
+              <li>Ratings</li>
+              <li>Shop Help</li>
+              <li>Contacs Us</li>
+            </ul>
+
+            <ul class="sites">
+              <li><h2>sites</h2></li>
+              <li>DC</li>
+              <li>MAD Magazine</li>
+              <li>DC Kids</li>
+              <li>DC Universe</li>
+              <li>DC Power Visa</li>
+            </ul>
+
+          </div>
+
+          <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="" />
+
+        </div>
+      </div>
+    </div>
+
+    <div class="bottom-footer-bg">
+      <div class="footer-container">
+        <div class="bottom-footer">
+
+          <button class="sign-up">sign-up now!</button>
+
+          <ul class="socials">
+            <li><h2>follow us</h2></li>
+            <li><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="facebook logo" /></li>
+            <li><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="twitter logo" /></li>
+            <li><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="youtube logo" /></li>
+            <li><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="pinterest logo" /></li>
+            <li><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="periscope logo" /></li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+  </footer>
