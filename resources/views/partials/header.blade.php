b707160197784f053de3c96b4b77c9ea87f1fda0
GianluGiarde <gianlugiarde@gmail.com>
2023-06-23 20:15:06 +0200

    Add: header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+<header>
+
+      <div class="logo">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC logo" />
+      </div>
+
+      <ul class="nav">
+        <li>charactes</li>
+        <li>comincs</li>
+        <li>movies</li>
+        <li>tv</li>
+        <li>games</li>
+        <li>collectibles</li>
+        <li>videos</li>
+        <li>fans</li>
+        <li>news</li>
+        <li>shop</li>
+      </ul>
+
+  </header>
